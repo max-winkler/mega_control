707a1a0d528bea08bf0ea291e9f3bdd8a2860990
Max Winkler <max.winkler@mathematik.tu-chemnitz.de>
2020-09-15 16:58:20 +0200

    Continued implementation of Allen-Cahn equation (not working yet)

--- AllenCahn.m
+++ AllenCahn.m
@@ -9,7 +9,7 @@ eps = 1.e-2;  % diffusion parameter
 T = 20;       % Final time
 
 % Subintervals per edge
-ne = 50;
+ne = 10;
 
 % Create graph object
 G = L_graph(5);
@@ -36,18 +36,19 @@ Ml = spdiags(Ml_diag,0,nall,nall);
 
 FreeNodes = 1:nf;
 
-nt=50;
+nt=100;
 tau = T/nt;
+C = spdiags([-ones(nt,1),ones(nt,1)], [0,1], nt-1, nt);
 
-y = zeros(nall, nt);
-y(FreeNodes,1) = rand(nf,1)-0.5; % Initial value
+Y = zeros(nall, nt);
+Y(FreeNodes,1) = rand(nf,1)-0.5; % Initial value
 
 % Implicit Euler
 % for i=1:nt-1
 %     
 %     fprintf("Time step %d of %d.\n", i, nt);
-%     y_new = y(FreeNodes,i);
-%     y_old = y(FreeNodes,i);
+%     y_new = Y(FreeNodes,i);
+%     y_old = Y(FreeNodes,i);
 %     
 %     % Newton method
 %     resid = 1.;
@@ -75,30 +76,57 @@ y(FreeNodes,1) = rand(nf,1)-0.5; % Initial value
 %         end
 %     end
 %         
-%     y(FreeNodes,i+1) = y_new;
+%     Y(FreeNodes,i+1) = y_new;
 % end
 
-% Routine for matrix-vector product in Newton system
-function b = DF(y,A,Ml,nall,nf,nt)
-    Y = reshape(y,nall,nt);
-    B = Ml(1:nf)*Y(1:nf,:)*C' + tau*eps*A(1:nf,:)*Yh(1:nf,:) + tau*diag(Ml(1:nf)).*(3*Y(1:nf,:).^2-1);
-    b = B(:);
-end
-
 % Newton Method
-resid = 1.
-while resid > 1.e-8        
-    F = [kron([ones(nt,1)],tau*M11*ones(nf,1));kron([ones(nt,1)],tau*M22*ones(nd,1));zeros(nt*nf,1)];
+resid = 1.;
+while resid > 1.e-8
+    F = Ml(1:nf,1:nf)*Y(1:nf,:)*C' ...
+        + tau*eps*K(1:nf,1:nf)*Y(1:nf,2:nt);
+    for i=1:nt-1
+        F(:,i) = F(:,i) + tau*Ml(1:nf,1:nf)*(Y(1:nf,i+1).^3 - Y(1:nf,i+1));
+    end
+    F = F(:);        
+    
+    resid = norm(F);
+    fprintf("  newton iteration, resid=%e\n", resid);
+    
+    if resid > 1.e-8   
+        % Solve Newton system        
+        %[dy,flag,relres,iter] = minres(@(x)DF(x,K,Ml,C,nall,nf,nt,tau), -F, 1e-8, 10000);
+        
+        DF = kron(C(:,2:nt)', Ml(1:nf,1:nf));
+        DF = DF + tau*eps*kron(speye(nt-1,nt-1), K(1:nf,1:nf));
+        
+        for i=1:nt-1
+            DF((i-1)*nf+1:i*nf,(i-1)*nf+1:i*nf) = DF((i-1)*nf+1:i*nf,(i-1)*nf+1:i*nf) ...
+                + tau*spdiags(Ml(1:nf,1:nf).*(3*Y(1:nf,i+1).^2-1),0,nf,nf);
+        end
+        
+        dy = DF\(-F);
+        
+        % Newton update        
+        dY = reshape(dy,nf,nt-1);
+        Y(1:nf,2:nt) = Y(1:nf,2:nt) + dY;
+    end            
 end
 
 % Play video
 MOV(nt+1) = struct('cdata',[],'colormap',[]);
 
 for l=1:nt
-    plot_function_over_graph(G,y(:,l));
+    plot_function_over_graph(G,Y(:,l));
     title(sprintf("Time step %d of %d", l, nt));
-    zlim([min(min(y)),max(max(y))]);
+    zlim([min(min(Y)),max(max(Y))]);
     MOV(l) = getframe(gca);
 end
 
 movie(MOV)
+
+% Routine for matrix-vector product in Newton system
+% function b = DF(y,A,Ml,C,nall,nf,nt,tau)    
+%     Y = reshape(y,nall,nt-1);
+%     B = Ml(1:nf,1:nf)*Y(1:nf,:)*C(:,2:nt)' + tau*eps*A(1:nf,1:nf)*Y(1:nf,:) + tau*diag(Ml(1:nf,1:nf)).*(3*Y(1:nf,:).^2-1);
+%     b = B(:);
+% end
